da556b431c06fb56726af9000fb9f4c9c1f9702c
Thiago Freitas <thiagoleonardo1995@live.com>
2022-10-06 11:48:32 -0300

    Calc func separadas

--- 06. Calculadora/js/main.js
+++ 06. Calculadora/js/main.js
@@ -1,4 +1,4 @@
-import * as operacoes from './services/operacoes.js'
+import * as operacoes from './services/operacoes.js';
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
@@ -23,18 +23,26 @@ function handleInputValor2Change(event) {
     console.log(valor2);
 }
 
-const somar = document.getElementById("btn-plus");
-somar.addEventListener('click', handleInputClique);
-const multiplicar = document.getElementById("btn-times");
-multiplicar.addEventListener('click', handleInputClique);
+const limpa = document.getElementById("btn-clear");
+limpa.addEventListener('click', handleInputClear);
+
+function handleInputClear(event) {
+    inputValor1.classList.remove("input-error");
+    inputValor1.value = "";
+    inputValor2.classList.remove("input-error");
+    inputValor2.value = "";
+    resultadoOperacao.innerHTML = 0;
+}
 
 const resultado = document.getElementById("result-box");
 const resultadoOperacao = document.createElement("p");
 resultadoOperacao.innerHTML = 0;
 resultado.appendChild(resultadoOperacao);
 
+const somar = document.getElementById("btn-plus");
+somar.addEventListener('click', handleInputSoma);
 
-function handleInputClique(event) {
+function handleInputSoma(event) {
     event.preventDefault;
     if (isNumber(valor1)) {
         valor1 = Number (valor1);
@@ -51,25 +59,62 @@ function handleInputClique(event) {
     }
 
     if (isNumber(valor1) && isNumber(valor2)) {
-        let resultadoF = 0;
-        if (somar) {
-            resultadoF = operacoes.somar(valor1,valor2);
-            resultadoOperacao.innerHTML = resultadoF;
-        } else {
-            resultadoF = operacoes.multiplicar(valor1,valor2);
-            resultadoOperacao.innerHTML = resultadoF;
-        }
+        resultadoOperacao.innerHTML = operacoes.somar(valor1,valor2);
     }
 };
 
-const limpa = document.getElementById("btn-clear");
-limpa.addEventListener('click', handleInputClear);
+const multiplicar = document.getElementById("btn-times");
+multiplicar.addEventListener('click', handleInputX);
 
-function handleInputClear(event) {
-    inputValor1.classList.remove("input-error");
-    inputValor1.value = "";
-    inputValor2.classList.remove("input-error");
-    inputValor2.value = "";
-    resultadoOperacao.innerHTML = 0;
-}
+function handleInputX(event) {
+    event.preventDefault;
+    if (isNumber(valor1)) {
+        valor1 = Number (valor1);
+        
+    } else {
+        inputValor1.classList.add("input-error");
+    }
+
+    if (isNumber(valor2)) {
+        valor2 = Number (valor2);
+        
+    } else {
+        inputValor2.classList.add("input-error");
+    }
+
+    if (isNumber(valor1) && isNumber(valor2)) {
+        resultadoOperacao.innerHTML = operacoes.multiplicar(valor1,valor2);
+    }
+};
+
+// function handleInputClique(event) {
+//     event.preventDefault;
+//     if (isNumber(valor1)) {
+//         valor1 = Number (valor1);
+        
+//     } else {
+//         inputValor1.classList.add("input-error");
+//     }
+
+//     if (isNumber(valor2)) {
+//         valor2 = Number (valor2);
+        
+//     } else {
+//         inputValor2.classList.add("input-error");
+//     }
+
+//     if (isNumber(valor1) && isNumber(valor2)) {
+//         let resultadoF = 0;
+//         if (somar) {
+//             resultadoF = operacoes.somar(valor1,valor2);
+//             resultadoOperacao.innerHTML += resultadoF;
+//         } else {
+//             resultadoF = "";
+//             resultadoF = operacoes.multiplicar(valor1,valor2);
+//             resultadoOperacao.innerHTML += resultadoF;
+//         }
+//     }
+// };
+
+
 
